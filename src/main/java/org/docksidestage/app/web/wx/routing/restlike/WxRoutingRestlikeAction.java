f592fe6f4b7e7543d5ab48eca98b27c9c3457bd0
jflute <dbflute@gmail.com>
2017-12-21 16:49:12 +0900

    add test for @Execute(urlPattern = "{}/@word/{}/@word/{}")

--- src/main/java/org/docksidestage/app/web/wx/routing/restlike/WxRoutingRestlikeAction.java
+++ src/main/java/org/docksidestage/app/web/wx/routing/restlike/WxRoutingRestlikeAction.java
@@ -19,32 +19,32 @@ public class WxRoutingRestlikeAction extends FortressBaseAction {
     //                                                 Index
     //                                                 -----
     // [hit]
-    // GET http://localhost:8151/fortress/wx/request/restlike/
-    // GET http://localhost:8151/fortress/wx/request/restlike/sea/
+    // GET http://localhost:8151/fortress/wx/routing/restlike/
+    // GET http://localhost:8151/fortress/wx/routing/restlike/sea/
     // [not]
-    // GET http://localhost:8151/fortress/wx/request/restlike/sea/land
-    @Execute // GET /wx/request/restlike/{first}/
+    // GET http://localhost:8151/fortress/wx/routing/restlike/sea/land
+    @Execute // GET /wx/routing/restlike/{first}/
     public JsonResponse<MaihamaBean> get$index(OptionalThing<String> first) {
         return asJson(new MaihamaBean("GET get$index()", first.orElse("*no value"), "*no value"));
     }
 
     // [hit]
-    // POST http://localhost:8151/fortress/wx/request/restlike/
-    @Execute // POST /wx/request/restlike/{first}/
+    // POST http://localhost:8151/fortress/wx/routing/restlike/
+    @Execute // POST /wx/routing/restlike/{first}/
     public JsonResponse<MaihamaBean> post$index(MaihamaForm form) {
         return asJson(new MaihamaBean("POST post$index()", "*no value", "*no value", form));
     }
 
     // [hit]
-    // PUT http://localhost:8151/fortress/wx/request/restlike/sea/
-    @Execute // POST /wx/request/restlike/{first}/
+    // PUT http://localhost:8151/fortress/wx/routing/restlike/sea/
+    @Execute // POST /wx/routing/restlike/{first}/
     public JsonResponse<MaihamaBean> put$index(String first, MaihamaForm form) {
         return asJson(new MaihamaBean("PUT put$index()", first, "*no value", form));
     }
 
     // [hit]
-    // DELETE http://localhost:8151/fortress/wx/request/restlike/sea/
-    @Execute // POST /wx/request/restlike/{first}/
+    // DELETE http://localhost:8151/fortress/wx/routing/restlike/sea/
+    @Execute // POST /wx/routing/restlike/{first}/
     public JsonResponse<MaihamaBean> delete$index(String first, MaihamaForm form) {
         return asJson(new MaihamaBean("DELETE delete$index()", first, "*no value", form));
     }
@@ -53,8 +53,8 @@ public class WxRoutingRestlikeAction extends FortressBaseAction {
     //                                             Operation
     //                                             ---------
     // [hit]
-    // GET http://localhost:8151/fortress/wx/request/restlike/sea/operation/
-    @Execute(urlPattern = "{}/@word") // GET /wx/request/restlike/{first}/operation/
+    // GET http://localhost:8151/fortress/wx/routing/restlike/sea/operation/
+    @Execute(urlPattern = "{}/@word") // GET /wx/routing/restlike/{first}/operation/
     public JsonResponse<MaihamaBean> get$operation(String first) {
         return asJson(new MaihamaBean("GET get$operation()", first, "*no value"));
     }
@@ -63,25 +63,35 @@ public class WxRoutingRestlikeAction extends FortressBaseAction {
     //                                                 Next
     //                                                ------
     // [hit]
-    // GET http://localhost:8151/fortress/wx/request/restlike/sea/next/land/
-    @Execute(urlPattern = "{}/@word/{}") // GET /wx/request/restlike/{first}/next/{second}
+    // GET http://localhost:8151/fortress/wx/routing/restlike/sea/next/land/
+    @Execute(urlPattern = "{}/@word/{}") // GET /wx/routing/restlike/{first}/next/{second}
     public JsonResponse<MaihamaBean> get$next(String first, String second) {
         return asJson(new MaihamaBean("GET", first, second));
     }
 
     // [hit]
-    // POST http://localhost:8151/fortress/wx/request/restlike/sea/next/land/
-    @Execute(urlPattern = "{}/@word/{}") // POST /wx/request/restlike/{first}/next/{second}
+    // POST http://localhost:8151/fortress/wx/routing/restlike/sea/next/land/
+    @Execute(urlPattern = "{}/@word/{}") // POST /wx/routing/restlike/{first}/next/{second}
     public JsonResponse<MaihamaBean> post$next(String first, String second, MaihamaNextForm form) {
         return asJson(new MaihamaBean("POST", first, second, form));
+    }
+
+    // -----------------------------------------------------
+    //                                             Next Next
+    //                                             ---------
+    // [hit]
+    // GET http://localhost:8151/fortress/wx/routing/restlike/sea/next/land/nextnext/piari
+    @Execute(urlPattern = "{}/@word/{}/@word/{}") // GET /wx/routing/restlike/{first}/next/{second}/nextnext/{third}
+    public JsonResponse<MaihamaBean> get$nextNextnext(String first, String second, String third) {
+        return asJson(new MaihamaBean("GET", first, second + " :: " + third));
     }
 
     // #hope now making
     //// -----------------------------------------------------
     ////                                         Last Optional
     ////                                         -------------
-    //// GET http://localhost:8151/fortress/wx/request/restlike/sea/lastopt/land/
-    //@Execute(urlPattern = "{}/@word/{}") // GET /wx/request/restlike/{first}/lastopt/{second}/
+    //// GET http://localhost:8151/fortress/wx/routing/restlike/sea/lastopt/land/
+    //@Execute(urlPattern = "{}/@word/{}") // GET /wx/routing/restlike/{first}/lastopt/{second}/
     //public JsonResponse<MaihamaBean> get$lastopt(String first, OptionalThing<String> second) {
     //    return asJson(new MaihamaBean("GET get$lastopt()", first, second.orElse("*empty value")));
     //}
