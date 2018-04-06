71f9a5495b75fdee3c51c30162c8fc0d5c7b6e5a
jflute <dbflute@gmail.com>
2018-04-06 19:18:03 +0900

    make WxExternalContextJobTest

---
+++ src/test/java/org/docksidestage/whitebox/lastadi/extctx/WxExternalContextJobTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.whitebox.lastadi.extctx;
+
+import org.docksidestage.unit.UnitFortressJobTestCase;
+import org.lastaflute.core.util.ContainerUtil;
+import org.lastaflute.web.util.LaRequestUtil;
+import org.lastaflute.web.util.LaResponseUtil;
+
+/**
+ * @author jflute
+ */
+public class WxExternalContextJobTest extends UnitFortressJobTestCase {
+
+    public void test_ContainerUtil() throws Exception {
+        assertFalse(ContainerUtil.hasExternalContext());
+        assertException(IllegalStateException.class, () -> ContainerUtil.retrieveExternalContext());
+    }
+
+    public void test_RequestUtil() throws Exception {
+        assertException(IllegalStateException.class, () -> LaRequestUtil.getRequest());
+        assertNotNull(LaRequestUtil.getOptionalRequest());
+        assertFalse(LaRequestUtil.getOptionalRequest().isPresent());
+    }
+
+    public void test_ResponseUtil() throws Exception {
+        assertException(IllegalStateException.class, () -> LaResponseUtil.getResponse());
+        assertNotNull(LaResponseUtil.getOptionalResponse());
+        assertFalse(LaResponseUtil.getOptionalResponse().isPresent());
+    }
+}
---
+++ src/test/java/org/docksidestage/whitebox/lastadi/extctx/WxExternalContextWebTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.whitebox.lastadi.extctx;
+
+import org.docksidestage.unit.UnitFortressWebTestCase;
+import org.lastaflute.core.util.ContainerUtil;
+import org.lastaflute.web.util.LaRequestUtil;
+import org.lastaflute.web.util.LaResponseUtil;
+
+/**
+ * @author jflute
+ */
+public class WxExternalContextWebTest extends UnitFortressWebTestCase {
+
+    public void test_ContainerUtil() throws Exception {
+        assertTrue(ContainerUtil.hasExternalContext());
+        assertNotNull(ContainerUtil.retrieveExternalContext());
+    }
+
+    public void test_RequestUtil() throws Exception {
+        assertNotNull(LaRequestUtil.getRequest());
+        assertNotNull(LaRequestUtil.getOptionalRequest());
+        assertTrue(LaRequestUtil.getOptionalRequest().isPresent());
+    }
+
+    public void test_ResponseUtil() throws Exception {
+        assertNotNull(LaResponseUtil.getResponse());
+        assertNotNull(LaResponseUtil.getOptionalResponse());
+        assertTrue(LaResponseUtil.getOptionalResponse().isPresent());
+    }
+}
