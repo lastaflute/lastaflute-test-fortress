23a4d07aa30871b0076a1a6ebb548186c9fbc247
jflute <jflute@jflute-nyabara>
2021-05-23 03:25:18 +0900

    remove todo, treat them as memo

--- src/main/java/org/docksidestage/app/web/wx/JFluteMemo.java
+++ src/main/java/org/docksidestage/app/web/wx/JFluteMemo.java
@@ -20,85 +20,85 @@ package org.docksidestage.app.web.wx;
  */
 public class JFluteMemo {
 
-    // TODO jflute lastaflute: [A] fitting: {}/@word/{} with OptionalThing parameter
-    // TODO jflute lastaflute: [A] fitting: ScalarResponse instead of JsonResponse<String>
-    // TODO jflute lastaflute: [A] fitting: StreamResponse can be treated as ApiResponse option?
-    // TODO jflute lastaflute: [A] fitting: ValidateTypeFailure for JSON
-    // TODO jflute lastaflute: [A] fitting: ValidateTypeFailure for List element
-    // TODO jflute lastaflute: [A] fitting: FormMappingOption provides date pattern
-    // TODO jflute lastaflute: [A] fitting: TypeLocalDate can specify date pattern
-    // TODO jflute lastaflute: [B] fitting: @Execute(camelToUrlElement=true)
-    // TODO jflute lastaflute: [B] fitting: Gson, Enum failure, null to exception
-    // TODO jflute lastaflute: [B] fitting: type-safe messages property
-    // TODO jflute lastaflute: [B] fitting: Form, Result, Optional property (related to Gson, Validator)
-    // TODO jflute lastaflute: [B] fitting: SlaveDB follow
-    // TODO jflute lastaflute: [B] fitting: throw 404 when No routing action by option?
-    // TODO jflute lastaflute: [B] fitting: Thymeleaf: render parts manager for Ajax
-    // TODO jflute lastaflute: [B] example: example of REST orleans
-    // TODO jflute lastaflute: [B] fitting: MailFlute, parameter property comment
-    // TODO jflute lastaflute: [B] fitting: Lasta Di, Di xml's expression new injection
-    // TODO jflute lastaflute: [B] fitting: Lasta DI, support lazy component
-    // TODO jflute lastaflute: [B] fitting: Lasta Job, Job Timeout
-    // TODO jflute lastaflute: [B] fitting: Lasta Job, Job Order
-    // TODO jflute lastaflute: [B] fitting: Lasta Job, Initial Boot
-    // TODO jflute lastaflute: [B] fitting: Lasta Job, Boot from Previous Boot
-    // TODO jflute lastaflute: [B] fitting: SSL: https://gist.github.com/taknb2nch/fdf048908ad98e6a8829ce9670e0bd0c
-    // TODO jflute lastaflute: [B] function: UTFlute stop directly entity for nested property
-    // TODO jflute lastaflute: [B] function: SessionSharedStorage, cookie example
-    // TODO jflute lastaflute: [B] fitting: JettyBoot SSL: https://gist.github.com/taknb2nch/fdf048908ad98e6a8829ce9670e0bd0c
-    // TODO jflute lastaflute: [B] document: LastaDoc, design and action driven
-    // TODO jflute lastaflute: [B] fitting: ??? LastaFilter: passing option
-    // TODO jflute lastaflute: [B] example: ??? use IndependentProcessor instead of Runnable
-    // TODO jflute lastaflute: [B] function: Form/Body/Bean property as OptionalThing
-    // TODO jflute lastaflute: [B] thinking: Form constructor mapping
-    // TODO jflute lastaflute: [B] fitting: improve message, TypeLocalDate for LocalDate (and javadoc)
-    // TODO jflute lastaflute: [B] fitting: TypeLocalDate's message {propertyType} cannot be resolved
+    // lastaflute: [A] fitting: {}/@word/{} with OptionalThing parameter
+    // lastaflute: [A] fitting: ScalarResponse instead of JsonResponse<String>
+    // lastaflute: [A] fitting: StreamResponse can be treated as ApiResponse option?
+    // lastaflute: [A] fitting: ValidateTypeFailure for JSON
+    // lastaflute: [A] fitting: ValidateTypeFailure for List element
+    // lastaflute: [A] fitting: FormMappingOption provides date pattern
+    // lastaflute: [A] fitting: TypeLocalDate can specify date pattern
+    // lastaflute: [B] fitting: @Execute(camelToUrlElement=true)
+    // lastaflute: [B] fitting: Gson, Enum failure, null to exception
+    // lastaflute: [B] fitting: type-safe messages property
+    // lastaflute: [B] fitting: Form, Result, Optional property (related to Gson, Validator)
+    // lastaflute: [B] fitting: SlaveDB follow
+    // lastaflute: [B] fitting: throw 404 when No routing action by option?
+    // lastaflute: [B] fitting: Thymeleaf: render parts manager for Ajax
+    // lastaflute: [B] example: example of REST orleans
+    // lastaflute: [B] fitting: MailFlute, parameter property comment
+    // lastaflute: [B] fitting: Lasta Di, Di xml's expression new injection
+    // lastaflute: [B] fitting: Lasta DI, support lazy component
+    // lastaflute: [B] fitting: Lasta Job, Job Timeout
+    // lastaflute: [B] fitting: Lasta Job, Job Order
+    // lastaflute: [B] fitting: Lasta Job, Initial Boot
+    // lastaflute: [B] fitting: Lasta Job, Boot from Previous Boot
+    // lastaflute: [B] fitting: SSL: https://gist.github.com/taknb2nch/fdf048908ad98e6a8829ce9670e0bd0c
+    // lastaflute: [B] function: UTFlute stop directly entity for nested property
+    // lastaflute: [B] function: SessionSharedStorage, cookie example
+    // lastaflute: [B] fitting: JettyBoot SSL: https://gist.github.com/taknb2nch/fdf048908ad98e6a8829ce9670e0bd0c
+    // lastaflute: [B] document: LastaDoc, design and action driven
+    // lastaflute: [B] fitting: ??? LastaFilter: passing option
+    // lastaflute: [B] example: ??? use IndependentProcessor instead of Runnable
+    // lastaflute: [B] function: Form/Body/Bean property as OptionalThing
+    // lastaflute: [B] thinking: Form constructor mapping
+    // lastaflute: [B] fitting: improve message, TypeLocalDate for LocalDate (and javadoc)
+    // lastaflute: [B] fitting: TypeLocalDate's message {propertyType} cannot be resolved
 
-    // TODO jflute lastaflute: [C] function: Gson, indefined property check
-    // TODO jflute lastaflute: [C] document: DI list, ContainerManagementServlet
-    // TODO jflute lastaflute: [C] fitting: Heroku, JDBC URL
-    // TODO jflute lastaflute: [C] fitting: LoginCredential, hibernate validator?
-    // TODO jflute lastaflute: [C] fitting: UTFlute, lonely new404()
-    // TODO jflute lastaflute: [C] fitting: ??? multipart, get submit error message
-    // TODO jflute lastaflute: [C] function: ??? original type mapping interface
-    // TODO jflute lastaflute: [C] fitting: ??? Form, Bean public field @Resource
-    // TODO jflute lastaflute: [C] function: ??? thymeleaf, optionMap?
+    // lastaflute: [C] function: Gson, indefined property check
+    // lastaflute: [C] document: DI list, ContainerManagementServlet
+    // lastaflute: [C] fitting: Heroku, JDBC URL
+    // lastaflute: [C] fitting: LoginCredential, hibernate validator?
+    // lastaflute: [C] fitting: UTFlute, lonely new404()
+    // lastaflute: [C] fitting: ??? multipart, get submit error message
+    // lastaflute: [C] function: ??? original type mapping interface
+    // lastaflute: [C] fitting: ??? Form, Bean public field @Resource
+    // lastaflute: [C] function: ??? thymeleaf, optionMap?
 
-    // TODO jflute lastaflute: [D] fitting: Mayaa: viewPrefix
-    // TODO jflute lastaflute: [D] fitting: LastaMixer2
-    // TODO jflute lastaflute: [D] function: getter way, validation error order
-    // TODO jflute lastaflute: [D] fitting: Taglib: errors escape
-    // TODO jflute lastaflute: [D] fitting: multipart, submit button name handling
-    // TODO jflute lastaflute: [D] fitting: action, form package OUT
-    // TODO jflute lastaflute: [D] fitting: improve AsyncManager
-    // TODO jflute lastaflute: [D] fitting: maven plugin for executable war
-    // TODO jflute lastaflute: [D] fitting: FreeGen maven execute performance tuning
-    // TODO jflute lastaflute: [D] function: HtmlFailureHook, StreamFailureHook
-    // TODO jflute lastaflute: [D] fitting: Lasta Di :: private AOP or exception
-    // TODO jflute lastaflute: [D] fitting: Hotdeploy, Action's inner class throws class cast exception in session
-    // TODO jflute lastaflute: [D] fitting: LastaTaglib :: la:number, la:date
-    // TODO jflute lastaflute: [D] fitting: Lasta Di :: throw exception if +component.xml's component not found
+    // lastaflute: [D] fitting: Mayaa: viewPrefix
+    // lastaflute: [D] fitting: LastaMixer2
+    // lastaflute: [D] function: getter way, validation error order
+    // lastaflute: [D] fitting: Taglib: errors escape
+    // lastaflute: [D] fitting: multipart, submit button name handling
+    // lastaflute: [D] fitting: action, form package OUT
+    // lastaflute: [D] fitting: improve AsyncManager
+    // lastaflute: [D] fitting: maven plugin for executable war
+    // lastaflute: [D] fitting: FreeGen maven execute performance tuning
+    // lastaflute: [D] function: HtmlFailureHook, StreamFailureHook
+    // lastaflute: [D] fitting: Lasta Di :: private AOP or exception
+    // lastaflute: [D] fitting: Hotdeploy, Action's inner class throws class cast exception in session
+    // lastaflute: [D] fitting: LastaTaglib :: la:number, la:date
+    // lastaflute: [D] fitting: Lasta Di :: throw exception if +component.xml's component not found
 
-    // TODO jflute lastaflute: [E] fitting: ActionAdjustment, ActionType URL filter for numeric URL
-    // TODO jflute lastaflute: [E] function: FreeGen for execute method and form property definition
-    // TODO jflute lastaflute: [E] fitting: nest JSON debug challenge
-    // TODO jflute lastaflute: [E] function: asRedirect(ProductListAciton.class).index(1);
-    // TODO jflute lastaflute: [E] fitting: want to judge validation target nested bean but no valid annotation
-    // TODO jflute lastaflute: [E] function: AsyncResponse for ElasticSearch? (future)
-    // TODO jflute lastaflute: [E] thinking: upload Servlet MultipartConfig
-    // TODO jflute lastaflute: [E] function: JSON bean item exitence check
-    // TODO jflute lastaflute: [E] function: x meta(form).memberName
-    // TODO jflute lastaflute: [E] fitting: jetty + tag files + hotdeploy problem, URLClassLoader, TagFileProcessor
-    // TODO jflute lastaflute: [E] fitting: Form,Body property OptionalThing? *thinking: Gson, HibernateValidator
-    // TODO jflute lastaflute: [E] improvement: Lasta DI :: redefiner, ++.xml logging path adjustment
-    // TODO jflute lastaflute: [E] improvement: Lasta DI :: cooldeploy-autoregister twice new? when cool, by +dicon?
+    // lastaflute: [E] fitting: ActionAdjustment, ActionType URL filter for numeric URL
+    // lastaflute: [E] function: FreeGen for execute method and form property definition
+    // lastaflute: [E] fitting: nest JSON debug challenge
+    // lastaflute: [E] function: asRedirect(ProductListAciton.class).index(1);
+    // lastaflute: [E] fitting: want to judge validation target nested bean but no valid annotation
+    // lastaflute: [E] function: AsyncResponse for ElasticSearch? (future)
+    // lastaflute: [E] thinking: upload Servlet MultipartConfig
+    // lastaflute: [E] function: JSON bean item exitence check
+    // lastaflute: [E] function: x meta(form).memberName
+    // lastaflute: [E] fitting: jetty + tag files + hotdeploy problem, URLClassLoader, TagFileProcessor
+    // lastaflute: [E] fitting: Form,Body property OptionalThing? *thinking: Gson, HibernateValidator
+    // lastaflute: [E] improvement: Lasta DI :: redefiner, ++.xml logging path adjustment
+    // lastaflute: [E] improvement: Lasta DI :: cooldeploy-autoregister twice new? when cool, by +dicon?
 
-    // TODO jflute lastaflute: [F] function: TimeManager typical business date
-    // TODO jflute lastaflute: [F] function: immutable Form, Body *thinking
-    // TODO jflute lastaflute: [F] function: post/get limitation, verifyHttpMethod()? annotation?
-    // TODO jflute lastaflute: [F] function: restful, for promotion
+    // lastaflute: [F] function: TimeManager typical business date
+    // lastaflute: [F] function: immutable Form, Body *thinking
+    // lastaflute: [F] function: post/get limitation, verifyHttpMethod()? annotation?
+    // lastaflute: [F] function: restful, for promotion
 
-    // TODO jflute lastaflute: [G] fitting: before checking index size, not to be out of memory *thinking
-    // TODO jflute lastaflute: [G] function: urlPattern="{}/{$class}/{$method}" *thinking
-    // TODO jflute lastaflute: [G] test: unit test for MDCListener, ActionFormMapperTest
+    // lastaflute: [G] fitting: before checking index size, not to be out of memory *thinking
+    // lastaflute: [G] function: urlPattern="{}/{$class}/{$method}" *thinking
+    // lastaflute: [G] test: unit test for MDCListener, ActionFormMapperTest
 }
