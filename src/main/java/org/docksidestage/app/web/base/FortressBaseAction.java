3555c7319becc9aa48a72785a119dbb54f800fb4
jflute <jflute@jflute-nyabara>
2021-06-17 16:27:00 +0900

    make example for Cross Login Bridge

--- src/main/java/org/docksidestage/app/web/base/FortressBaseAction.java
+++ src/main/java/org/docksidestage/app/web/base/FortressBaseAction.java
@@ -23,6 +23,7 @@ import org.docksidestage.app.logic.context.AccessContextLogic;
 import org.docksidestage.app.logic.i18n.I18nDateLogic;
 import org.docksidestage.app.web.base.csrf.CsrfTokenAssist;
 import org.docksidestage.app.web.base.login.FortressLoginAssist;
+import org.docksidestage.bizfw.crosslogin.CrossLoginBridge;
 import org.docksidestage.mylasta.action.FortressHtmlPath;
 import org.docksidestage.mylasta.action.FortressMessages;
 import org.docksidestage.mylasta.action.FortressUserBean;
@@ -60,6 +61,8 @@ public abstract class FortressBaseAction extends TypicalAction // has several in
     private RequestManager requestManager;
     @Resource
     private FortressConfig fortressConfig;
+    @Resource
+    private CrossLoginBridge crossLoginTransfer;
     @Resource
     private FortressLoginAssist loginAssist;
     @Resource
@@ -107,6 +110,7 @@ public abstract class FortressBaseAction extends TypicalAction // has several in
     @Override
     public ActionResponse hookBefore(ActionRuntime runtime) { // application may override
         csrfTokenAssist.hookBefore(runtime);
+        crossLoginTransfer.transfer(APP_TYPE, getUserBean(), USER_TYPE); // for e.g. RemoteApi
         return super.hookBefore(runtime);
     }
 
---
+++ src/main/java/org/docksidestage/bizfw/crosslogin/CrossLoginBridge.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.bizfw.crosslogin;
+
+import org.dbflute.optional.OptionalThing;
+import org.lastaflute.core.magic.ThreadCacheContext;
+import org.lastaflute.web.login.UserBean;
+
+/**
+ * @author jflute
+ */
+public class CrossLoginBridge {
+
+    private static final String KEY_CROSS_LOGIN = "app:crossLogin";
+
+    public void transfer(String appType, OptionalThing<? extends UserBean<? extends Number>> userBean, String userType) { // called by e.g. hookBefore()
+        OptionalThing<? extends Number> userId = userBean.map(bean -> bean.getUserId());
+        CrossLoginBean crossLoginBean = new CrossLoginBean(appType, userId, userType);
+        ThreadCacheContext.setObject(KEY_CROSS_LOGIN, crossLoginBean);
+    }
+
+    public OptionalThing<CrossLoginBean> retrieve() { // called by e.g. RemoteApi's default rule
+        String key = KEY_CROSS_LOGIN;
+        CrossLoginBean crossLoginBean = ThreadCacheContext.getObject(key);
+        return OptionalThing.ofNullable(crossLoginBean, () -> {
+            throw new IllegalStateException("Not found the cross login bean: " + key);
+        });
+    }
+}
