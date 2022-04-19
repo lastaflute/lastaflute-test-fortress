3b3d18b5e2f3bcdeaea42276b29e32f55f2c8eb2
jflute <jflute@jflute-dorachan>
2022-04-20 00:22:28 +0900

    make LastaMeta test (e.g. egDefaultValue)

---
+++ src/main/java/org/docksidestage/app/web/wx/lastameta/WxLastametaAction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.app.web.wx.lastameta;
+
+import org.docksidestage.app.web.base.FortressBaseAction;
+import org.lastaflute.web.Execute;
+import org.lastaflute.web.login.AllowAnyoneAccess;
+import org.lastaflute.web.response.JsonResponse;
+
+/**
+ * This is the first line of class java-doc. <br>
+ * And the second line is here.
+ * @author jflute
+ */
+@AllowAnyoneAccess
+public class WxLastametaAction extends FortressBaseAction {
+
+    // http://localhost:8151/fortress/wx/lastameta/
+    @Execute
+    public JsonResponse<WxLastametaBody> index(WxLastametaBody body) {
+        return asJson(body);
+    }
+}
