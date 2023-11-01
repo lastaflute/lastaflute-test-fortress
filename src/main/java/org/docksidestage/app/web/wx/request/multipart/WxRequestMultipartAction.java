72c9f67fda2394863cd0baf7388d386cfa837001
jflute <dbflute@gmail.com>
2023-11-01 15:27:21 +0900

    improve multipart action for detail test

--- src/main/java/org/docksidestage/app/web/wx/request/multipart/WxRequestMultipartAction.java
+++ src/main/java/org/docksidestage/app/web/wx/request/multipart/WxRequestMultipartAction.java
@@ -17,11 +17,16 @@ package org.docksidestage.app.web.wx.request.multipart;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
+
 import org.docksidestage.app.web.base.FortressBaseAction;
+import org.docksidestage.mylasta.direction.sponsor.FortressMultipartRequestHandler;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.login.AllowAnyoneAccess;
 import org.lastaflute.web.response.HtmlResponse;
 import org.lastaflute.web.ruts.multipart.MultipartFormFile;
+import org.lastaflute.web.ruts.multipart.exception.MultipartExceededException;
+import org.lastaflute.web.servlet.request.RequestManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,30 +36,55 @@ import org.slf4j.LoggerFactory;
 @AllowAnyoneAccess
 public class WxRequestMultipartAction extends FortressBaseAction {
 
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
     private static final Logger logger = LoggerFactory.getLogger(WxRequestMultipartAction.class);
 
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    @Resource
+    private RequestManager requestManager;
+
+    // ===================================================================================
+    //                                                                             Execute
+    //                                                                             =======
     // http://localhost:8151/fortress/wx/request/multipart/
     @Execute
     public HtmlResponse index() {
+        System.gc();
         return asHtml(path_WxMultipart_WxMultipartHtml);
     }
 
     @Execute
     public HtmlResponse upload(WxRequestMultipartForm form) {
         validate(form, messages -> {}, () -> {
+            // the uploadedFile is required property so it comes here when exceeded
+            handleExceededException(form);
             return asHtml(path_WxMultipart_WxMultipartHtml);
         });
         showUploadedFile(form);
         return asHtml(path_WxMultipart_WxMultipartHtml);
     }
 
+    private void handleExceededException(WxRequestMultipartForm form) {
+        String key = FortressMultipartRequestHandler.MAX_LENGTH_EXCEEDED_KEY;
+        requestManager.getAttribute(key, MultipartExceededException.class).ifPresent(cause -> {
+            logger.debug("*Found the exceeded file: " + form, cause); // logging only here
+        });
+    }
+
     private void showUploadedFile(WxRequestMultipartForm form) {
         MultipartFormFile uploadedFile = form.uploadedFile;
+        if (uploadedFile == null) { // for temporary test
+            return;
+        }
         logger.debug("/- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ");
         try {
             String fileName = uploadedFile.getFileName();
             String fileData = new String(uploadedFile.getFileData(), "UTF-8");
-            logger.debug("#upload: {}, {}, {}={}", form.sea, form.land, fileName, fileData);
+            logger.debug("#upload: {}, {}, {}={}", form.sea, form.land, fileName, fileData.length());
         } catch (IOException e) {
             throw new IllegalStateException("Failed to show file data: " + uploadedFile, e);
         }
