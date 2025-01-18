a7dcc7bd6135430d069a736252d7e9e7265d96d5
jflute <dbflute@gmail.com>
2025-01-18 10:29:24 +0900

    mockのunittestとりあえず正常系だけ

--- src/test/java/org/docksidestage/mylasta/direction/sponsor/planner/rabbitmq/AllRabbitMQPlannerTest.java
+++ src/test/java/org/docksidestage/mylasta/direction/sponsor/planner/rabbitmq/AllRabbitMQPlannerTest.java
@@ -2,12 +2,17 @@ package org.docksidestage.mylasta.direction.sponsor.planner.rabbitmq;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
 import org.docksidestage.mylasta.direction.FortressConfig;
 import org.docksidestage.unit.UnitFortressJobTestCase;
+import org.lastaflute.job.JobManager;
+import org.lastaflute.job.LaJobHistory;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.DeliverCallback;
@@ -21,6 +26,8 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
 
     @Resource
     private FortressConfig config;
+    @Resource
+    private JobManager jobManager;
 
     // ===================================================================================
     //                                                                            Settings
@@ -46,16 +53,17 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
     //                                                                              ======
     public void test_bootAllConsumer_bootOnly() {
         // ## Arrange ##
-        Set<String> calledSet = newHashSet();
+        Set<String> calledSet = createCallerSet();
         AllRabbitMQPlanner planner = createMockAllRabbitMQPlanner(calledSet);
 
         // ## Act ##
         planner.bootAllConsumer();
 
         // ## Assert ##
-        sleep(5000); // 待たないとログが出ないので
+        sleep(2000); // 待たないとログが出ないので
 
         // cannot assert order because of asynchronous
+        log("calledSet: {}", calledSet);
         assertEquals(4, calledSet.size());
         calledSet.contains("queueDeclare(): seaQueue");
         calledSet.contains("queueDeclare(): landQueue");
@@ -63,20 +71,24 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
         calledSet.contains("basicConsume(): landQueue");
 
         // and visual check, confirm that consumer bootings are correct on log
+        List<LaJobHistory> jobHistoryList = jobManager.searchJobHistoryList();
+        log("jobHistory: {}", jobHistoryList);
+        assertHasZeroElement(jobHistoryList); // because of booting only
     }
 
     public void test_bootAllConsumer_withCallback() {
         // ## Arrange ##
-        Set<String> calledSet = newHashSet();
+        Set<String> calledSet = createCallerSet();
         AllRabbitMQPlanner planner = createMockAllRabbitMQPlannerWithCallback(calledSet);
 
         // ## Act ##
         planner.bootAllConsumer();
 
         // ## Assert ##
-        sleep(5000); // 待たないとログが出ないので
+        sleep(2000); // 待たないとログが出ないので
 
         // cannot assert order because of asynchronous
+        log("calledSet: {}", calledSet);
         assertEquals(4, calledSet.size());
         calledSet.contains("queueDeclare(): seaQueue");
         calledSet.contains("queueDeclare(): landQueue");
@@ -84,8 +96,29 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
         calledSet.contains("basicConsume(): landQueue");
 
         // and visual check, confirm that two jobs are executed on log
+        List<LaJobHistory> jobHistoryList = jobManager.searchJobHistoryList();
+        log("jobHistory:");
+        for (LaJobHistory jobHistory : jobHistoryList) {
+            log("  " + jobHistory);
+        }
+        assertEquals(2, jobHistoryList.size());
+        List<String> uniqueList = jobHistoryList.stream().map(history -> {
+            return history.getJobUnique().get().value();
+        }).collect(Collectors.toList());
+        assertTrue(uniqueList.contains(AllRabbitMQPlanner.mysticJobUnique));
+        assertTrue(uniqueList.contains(AllRabbitMQPlanner.onemanJobUnique));
     }
 
+    // ===================================================================================
+    //                                                                         Test Helper
+    //                                                                         ===========
+    private CopyOnWriteArraySet<String> createCallerSet() {
+        return new CopyOnWriteArraySet<>(); // used in multi-thread
+    }
+
+    // -----------------------------------------------------
+    //                                               Planner
+    //                                               -------
     private AllRabbitMQPlanner createMockAllRabbitMQPlanner(Set<String> calledSet) {
         return doCreateMockAllRabbitMQPlanner(calledSet, /*withCallback*/false);
     }
@@ -111,15 +144,37 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
         return mocker.createMockAllRabbitMQPlanner();
     }
 
+    // -----------------------------------------------------
+    //                                       DeliverCallback
+    //                                       ---------------
     private void executeCallback(String queue, DeliverCallback deliverCallback) {
         Envelope envelope = new Envelope(1L, false, queue + "Exchange", queue + "routingKey");
 
+        String bodyText = prepareBodyText(queue);
+        byte[] bodyBytes = toBodyBytes(bodyText);
+
+        Delivery message = new Delivery(envelope, new BasicProperties(), bodyBytes);
+        try {
+            String consumerTag = queue + "Tag"; // tekito
+            deliverCallback.handle(consumerTag, message);
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to handle the deliver callback: envelope=" + envelope, e);
+        }
+    }
+
+    private String prepareBodyText(String queue) {
         String bodyText;
         if (queue.startsWith("sea")) {
             bodyText = "{stageName: \"hangar\", oneDayShowCount: 5}";
-        } else { // land
+        } else if (queue.startsWith("land")) {
             bodyText = "{stageName: \"showbase\", showLastDate: \"2019-12-13\"}";
+        } else {
+            throw new IllegalStateException("Unknown queue: " + queue);
         }
+        return bodyText;
+    }
+
+    private byte[] toBodyBytes(String bodyText) {
         String encoding = "UTF-8";
         byte[] bodyBytes;
         try {
@@ -127,12 +182,6 @@ public class AllRabbitMQPlannerTest extends UnitFortressJobTestCase {
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException("Failed to get bytes: encoding=" + encoding, e);
         }
-
-        Delivery message = new Delivery(envelope, new BasicProperties(), bodyBytes);
-        try {
-            deliverCallback.handle(queue + "Tag", message);
-        } catch (IOException e) {
-            throw new IllegalStateException("Failed to handle the deliver callback: envelope=" + envelope, e);
-        }
+        return bodyBytes;
     }
 }
